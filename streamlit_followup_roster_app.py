f76d255aa300133f30d90aa4d627c959ee1edf34
umarklappe-pixel <umar.klappe@gmail.com>
2025-09-14 23:10:22 +0500

    Update streamlit_followup_roster_app.py

--- streamlit_followup_roster_app.py
+++ streamlit_followup_roster_app.py
@@ -102,7 +102,7 @@ if page == "Follow-up Form":
 
             add_followup({
                 "timestamp": timestamp,
-                "equipment": equipment-equipment_no,
+                "equipment": equipment + "-" + equipment_no,
                 "section": section,
                 "location": location,
                 "issue": issue,
